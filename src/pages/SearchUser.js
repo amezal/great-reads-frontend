e64c272caf023bd800750d5ef85d4a536213e69b
Armando <amezal01@hotmail.com>
2022-04-16 21:08:16 -0600

    Search user page

--- src/pages/SearchUser.js
+++ src/pages/SearchUser.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useSearchParams, useNavigate, Link } from 'react-router-dom';
+import { Group, Text, Paper, Container, Avatar } from '@mantine/core';
 import axios from 'axios';
 
 function SearchUser() {
@@ -18,23 +19,33 @@ function SearchUser() {
 
 
   return (
-    <div>
-      {
-        (data && data.length !== 0) ?
-          <>
-            {data.map(user => (
-              <div key={user._id}>
-                <h3>{user.name}</h3>
-                <img src={user.picture} alt="" />
-              </div>
-            ))}
-          </>
-          :
-          <>
-            <h2>No users found</h2>
-          </>
+    <Container size="xs" style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', gap: '1rem' }}>
+      {(data && data.length !== 0) ?
+        <>
+          {data.map(user => (
+            <Link key={user._id} to={`/users/${user._id}`} style={{ textDecoration: 'none' }}>
+              <Paper radius={12} p={12} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexWrap: 'nowrap' }}
+                sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.blue[5], })}
+              >
+                {/* <BookCover book={book} size="M" /> */}
+                <Avatar src={user.picture} size={52} alt="logo" radius={12} />
+                <Group direction="column" align="flex-start" spacing={0} ml={24}>
+                  <Text color="gray" size="xl" >
+                    {user.name}
+                  </Text>
+                  <Group direction="row">
+                    <Text color="gray" size="xs" style={{ fontStyle: 'italic' }}>Read: {user.books.read.length}</Text>
+                    <Text color="gray" size="xs" style={{ fontStyle: 'italic' }}>Want to read: {user.books.want.length}</Text>
+                    <Text color="gray" size="xs" style={{ fontStyle: 'italic' }}>Reading: {user.books.reading.length}</Text>
+                  </Group>
+                </Group>
+              </Paper>
+            </Link>
+          ))}
+        </> :
+        <h1></h1>
       }
-    </div>
+    </Container>
   )
 
 }
